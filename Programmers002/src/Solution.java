5a6c56b63f5e0f7c6f81da73925326a342e970d2
yerim <yerim609@gmail.com>
2021-03-22 15:22:50 +0900

    Java1-2
    크레인 인형뽑기 게임

--- Programmers002/src/Solution.java
+++ Programmers002/src/Solution.java
@@ -1,28 +1,83 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Solution {
-    public static int solution(int[][] board, int[] moves) {
+class Solution {
+
+	public static int solution(int[][] board, int[] moves) {
         int answer = 0;
+        ArrayList<Integer> basket = new ArrayList<>();
+        int row = board.length;
+        int col = board[0].length;
         
+		for(int i=0; i<row; i++) {
+			for(int j=0; j<col; j++) {
+				if(board[i][j]<0||board[i][j]>100) {
+					System.out.println("board의 각 칸에는 0 이상 100 이하인 정수를 담아야됩니다.");
+					System.exit(1);
+				}
+			}
+		}
+		if(row<5||row>30||col<5||row>30) {
+			System.out.println("5x5 이상 30x30 이하로 입력해주세요.");
+			System.exit(1);
+		}
+		if(moves.length<1||moves.length>1000) {
+			System.out.println("moves 배열의 크기는 1이상 1,000이하입니다.");
+			System.exit(1);
+		}
+		for(int i=0; i<moves.length; i++) {
+			if(moves[i]<1||moves[i]>col) {
+				System.out.println("moves 배열의 각 원소들의 값은 1이상이며 board 배열의 가로 크기 이하인 자연수여야 합니다.");
+				System.exit(1);
+			}
+		}
         
+        for(int i=0; i<moves.length; i++) {
+        	int num = moves[i];
+        	for(int j=0; j<board.length; j++) {
+            	if(board[j][num-1] != 0) {
+            		basket.add(board[j][num-1]);
+            		board[j][num-1] = 0;
+            		break;
+            	}
+        	}
+        }
+        
+        for(int i=0;i<basket.size()-1; i++) {
+        	if(basket.get(i)==basket.get(i+1)) {
+        		basket.remove(i);
+        		basket.remove(i);
+        		answer+=2;
+        		i=-1;
+        	}
+        }
         
         return answer;
     }
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
-		String in = scanner.nextLine(); // board에 들어갈 수
-		String moveIn = scanner.nextLine(); // moves에 들어갈 수
-		int len = in.length();
+		int answer = 0;
 		int start = 2;
 		int end = 0;
 		int line = 0;
 		int row=0, col=0;
+		String input = scanner.nextLine();
+		String in = "", moveIn = ""; // in: board에 들어갈 수, moveIn: moves에 들어갈 수
+		
+		for(int i=0; i<input.length()-1; i++) {
+			if((input.charAt(i)+"").equals("]")&&(input.charAt(i+1)+"").equals("]")) {
+				in = input.substring(0,i+2);
+				moveIn = input.substring(i+4,input.length());
+				break;
+			}
+		}
+		
+		int len = in.length();
 		
 		for(int i=2; i<len-1; i++) { // 열 수 세기
 			if((in.charAt(i)+"").equals("]")) {
-				col = (i-1)/2;
+				col = (i)/3;
 				break;
 			}
 		}
@@ -32,6 +87,8 @@ public class Solution {
 			}
 		}
 		
+
+		
 		int[][] board = new int[row][col];
 		int[] moves = new int[moveIn.length()];
 		
@@ -40,26 +97,24 @@ public class Solution {
 			if((in.charAt(i)+"").equals("]")) {
 				end = i;
 				String s = in.substring(start, end);
-				start = end+3;
-				String temp[] = s.split(",");
+				start = end+4;
+				String temp[] = s.split(", ");
 				int oneLine[] = toIntArray(temp);
 				board[line++] = oneLine;
 			}
 		}
 		
+
+		
 		/* moveIn -> moves */
 		moveIn = moveIn.substring(1, moveIn.length()-1);
-		String temp[] = moveIn.split(",");
+		String temp[] = moveIn.split(", ");
 		moves = toIntArray(temp);
 		
-//		for(int i=0; i<row; i++) {
-//			for(int j=0; j<col; j++) {
-//				System.out.print(board[i][j]);
-//			}
-//			System.out.println();
-//		}
+
 		
-		solution(board, moves);
+		answer = solution(board, moves);
+		System.out.print(answer);
 		
 	}
 	
