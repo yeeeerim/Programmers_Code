7d0592445c97f1b817bb3d0a5508fdbf3a7cd7bf
yerim <pppp5097@naver.com>
2021-03-29 20:22:53 +0900

    Java1-19
    서울에서 김서방 찾기

---
+++ Programmers019/src/Solution.java
@@ -0,0 +1,14 @@
+
+public class Solution {
+    public static String solution(String[] seoul) {
+        for(int i=0; i<seoul.length; i++) {
+        	if(seoul[i].equals("Kim")) return "김서방은 "+i+"에 있다";
+        }
+        return "";
+    }
+	public static void main(String[] args) {
+		String[] seoul = {"Jane", "Kim"};
+		System.out.println(solution(seoul));
+	}
+
+}
