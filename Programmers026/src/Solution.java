0e9cd87328b67b88e02db1980eec0926f1aa039b
yerim <pppp5097@naver.com>
2021-04-07 22:45:35 +0900

    Java1-26
    이상한 문자 만들기

--- Programmers026/src/Solution.java
+++ Programmers026/src/Solution.java
@@ -3,20 +3,6 @@ import java.util.ArrayList;
 public class Solution {
     public static String solution(String s) {
     	String answer="";
-//    	ArrayList<Integer> blank = new ArrayList<>();
-//    	for(int i=0; i<s.length(); i++) {
-//    		if((s.charAt(i)+"").equals(" ")) blank.add(i);
-//    	}
-//    	
-//    	String[] word = s.split(" ");
-//    	
-//    	for(int i=0; i<word.length; i++) {
-//    		for(int j=0; j<word[i].length(); j++) {
-//            	if(j%2==0) answer+=Character.toUpperCase(word[i].charAt(j));
-//            	else answer+=Character.toLowerCase(word[i].charAt(j));
-//    		}
-//    		answer+=" ";
-//    	}
     	int index=0;
     	int i=0;
     	while(true) {
@@ -32,7 +18,6 @@ public class Solution {
     		if(i==s.length()) break;
     	}
     	return answer;
-
     }
     public static void main(String args[]) {
     	System.out.println(solution("try     hello world   "));
