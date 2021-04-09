2a673dbd174a8d59ffb8f1ce87251e15183728ce
yerim <pppp5097@naver.com>
2021-04-09 23:47:08 +0900

    Java1-32
    짝수와 홀수

---
+++ Programmers032/src/Solution.java
@@ -0,0 +1,11 @@
+
+public class Solution {
+    public static String solution(int num) {
+        if(num%2==0) return "Even";
+        return "Odd";
+    }
+	public static void main(String[] args) {
+		System.out.println(solution(3));
+	}
+
+}
