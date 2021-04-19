e9f13f47d8dde1b376ea98ec43a6aa637343b94a
yerim <pppp5097@naver.com>
2021-04-19 22:22:41 +0900

    Java1-47
    진행중

---
+++ Programmers047/src/Solution.java
@@ -0,0 +1,21 @@
+
+public class Solution {
+    public int solution(String dartResult) {
+        int answer = 0;
+        int[] arr = new int[3];
+        
+        for(int i=0; i<dartResult.length(); i++) {
+        	if(dartResult.substring(i, i+1).equals("*")||dartResult.substring(i, i+1).equals("#")) {
+        		
+        	}
+        }
+        
+        
+        return answer;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1S2D*3T");
+	}
+
+}
