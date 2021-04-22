20cc61f36eb369c6b6b7a8a55ba33be345b746cd
yerim <pppp5097@naver.com>
2021-04-22 22:53:28 +0900

    Java1-47
    [1차] 다트 게임

--- Programmers047/src/Solution.java
+++ Programmers047/src/Solution.java
@@ -1,21 +1,35 @@
 
 public class Solution {
-    public int solution(String dartResult) {
+    public static int solution(String dartResult) {
         int answer = 0;
         int[] arr = new int[3];
+        String s = "";
         
         for(int i=0; i<dartResult.length(); i++) {
-        	if(dartResult.substring(i, i+1).equals("*")||dartResult.substring(i, i+1).equals("#")) {
-        		
+        	if(!dartResult.substring(i, i+1).equals("*")&&!dartResult.substring(i, i+1).equals("#")) {
+        		s+=dartResult.substring(i,i+1);
         	}
         }
         
+        int index=0;
+        
+        for(int i=0; i<s.length(); i+=2) {
+        	int temp = Integer.parseInt(s.substring(i, i+1));
+        	switch(s.substring(i+1, i+2)) {
+	        	case "S": temp*=1; break;
+	        	case "D": temp*=2; break;
+	        	case "T": temp*=3; break;
+        	}
+        	arr[index++] = temp;
+        }
+        
+//        for(int i=0;i<3;i++)
+//        	System.out.println(arr[i]);
         
         return answer;
     }
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("1S2D*3T");
+		System.out.println(solution("1S2D*3T"));
 	}
 
 }
