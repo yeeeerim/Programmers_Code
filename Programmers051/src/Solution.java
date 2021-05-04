ef6dc4667544c53efc0fbbc6309fc872a4d3333a
yerim <pppp5097@naver.com>
2021-05-04 23:37:01 +0900

    Java2-4
    다리를 지나는 트럭
    (5번 시간 초과)

--- Programmers051/src/Solution.java
+++ Programmers051/src/Solution.java
@@ -2,38 +2,36 @@ import java.util.ArrayList;
 
 public class Solution {
     public static int solution(int bridge_length, int weight, int[] truck_weights) {
-        ArrayList<Integer> tw = new ArrayList<>(truck_weights.length);
         ArrayList<Integer> ing = new ArrayList<>(bridge_length);
         int sum = truck_weights[0];
         int cnt = 2;
+        int index=0;
         
-        if(bridge_length==1) return 2;
-        if(truck_weights.length==1) return weight+1;
+        if(bridge_length==1) return truck_weights.length+1;
+        if(truck_weights.length==1) return bridge_length+1;
         
-        for(int i=0; i<truck_weights.length; i++)
-        	tw.add(truck_weights[i]);
         
-        ing.add(tw.remove(0));
+        ing.add(truck_weights[index++]);
         
         for(int i=0; i<bridge_length-1; i++)
         	ing.add(0);
         
-        while(tw.size()!=0) {
+        while(index<truck_weights.length) {
         	cnt++;
-        	ing.add(0,tw.get(0));
-    		sum+=tw.get(0); if(cnt>bridge_length) sum-=ing.get(bridge_length);
-        	if(sum<=weight) {
-        		tw.remove(0);
+        	ing.add(0,truck_weights[index]);
+    		if(cnt>bridge_length) sum-=ing.get(bridge_length);
+        	if(sum+truck_weights[index]<=weight) { // 다음 트럭이 다리에 올라올 수 있으면;
+        		sum+=truck_weights[index++]; 
         	} else {
-        		sum-=tw.get(0);
         		ing.remove(0);
         		ing.add(0,0);
         	}
         }
+        
         return ing.size()+1;
     }
     public static void main(String args[]) {
-    	int tw[] = {7,4,5,6};
+    	int[] tw = {7,4,5,6};
     	System.out.println(solution(2,10,tw));
     }
 }
