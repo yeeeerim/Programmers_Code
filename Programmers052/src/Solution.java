9783b3806305cd98bfa3ad01af8c46ce0e6f7e06
yerim <pppp5097@naver.com>
2021-05-06 20:07:02 +0900

    Java2-5
    문자열 압축

--- Programmers052/src/Solution.java
+++ Programmers052/src/Solution.java
@@ -2,36 +2,40 @@ import java.util.ArrayList;
 
 public class Solution {
     public static int solution(String s) {
-        int answer = 0;
         int index = 0;
         ArrayList<String> arr = new ArrayList<>();
-        String str = "";
+        int minCnt = 1001;
         
         
-        for(int i=1; i<s.length()/2; i++) {
-        	for(int j=0; j<s.length()/i; j++) {
-        		arr.add(s.substring(index, index+i));
-        		index++;
+        for(int i=1; i<=(s.length()==1?1:s.length()/2); i++) {
+        	arr = new ArrayList<>();
+            String str = "";
+            index=0;
+        	for(int j=0; j<Math.ceil((double)s.length()/i); j++) {
+        		arr.add(s.substring(index, Math.min(index+i, s.length())));
+        		index+=i;
         	}
-        	for(int j=0; j<s.length(); j++) {
+        	for(int j=0; j<arr.size(); j++) {
         		int cnt = 1;
         		String comp = arr.get(j);
-        		while(arr.get(j+1).equals(comp)) {
-        			cnt++; j++;
-        			if(j==s.length()-1) break;
+        		if(j==arr.size()-1) {
+        			str+=comp;
+        		} else {
+            		while(arr.get(j+1).equals(comp)) {
+            			cnt++; j++;
+            			if(j==arr.size()-1) break;
+            		}
+            		if(cnt==1) str+=comp;
+            		else str=str+cnt+comp;
         		}
-        		if(cnt==1) str+=comp;
-        		else str=str+cnt+comp;
         	}
-        	System.out.println(str);
-        	return str.length();
+        	if(minCnt>str.length()) minCnt = str.length();
         }
         
-        return answer;
+        return minCnt;
     }
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(solution("aabbaccc"));
+		System.out.println(solution("a"));
 	}
 
 }
