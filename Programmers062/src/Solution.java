1c3941173b2acd996342cc01bcc0c7f300d670b1
yerim <pppp5097@naver.com>
2021-05-25 17:14:51 +0900

    Java2-15
    행렬의 곱셈

--- Programmers062/src/Solution.java
+++ Programmers062/src/Solution.java
@@ -1,7 +1,7 @@
 
 public class Solution {
     public static int[][] solution(int[][] arr1, int[][] arr2) {
-        int[][] answer = new int[Math.max(arr1.length, arr2.length)][Math.max(arr1[0].length, arr2[0].length)];
+        int[][] answer = new int[arr1.length][arr2[0].length];
         
         for(int i=0; i<arr1.length; i++) {
         	for(int j=0; j<arr2[0].length; j++) {
