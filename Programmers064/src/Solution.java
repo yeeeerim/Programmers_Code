4d4dee7ed09b8edba75305bafb7a8731e779f2bb
yerim <pppp5097@naver.com>
2021-05-28 23:30:39 +0900

    Java2-17
    JadenCase 문자열 만들기

--- Programmers064/src/Solution.java
+++ Programmers064/src/Solution.java
@@ -1,24 +1,24 @@
 
 public class Solution {
     public static String solution(String s) {
-        String answer = "";
+        String answer = s.toLowerCase();
+        boolean check = true;
         
-        String[] str = s.split(" ");
-        for(String k : str) {
-        	String temp = k.toLowerCase();
-        	char first = temp.charAt(0);
-        	if(first>=97&&first<=122) {
-        		String uc = (first+"").toUpperCase();
-        		temp = temp.substring(1, temp.length());
-        		temp = uc+temp;
+        for(int i=0; i<answer.length(); i++) {
+        	while(answer.charAt(i)==32) {
+        		check=true;
+        		if((i++)==answer.length()-1) return answer;
         	}
-        	answer+=(temp+" ");
+        	if(check&&answer.charAt(i)>=97&&answer.charAt(i)<=122) {
+        		answer = answer.substring(0, i) + answer.substring(i, i+1).toUpperCase() + answer.substring(i+1);
+        	}
+        	check = false;
         }
         
-        return answer.substring(0, answer.length()-1);
+        return answer;
     }
 	public static void main(String[] args) {
-		System.out.println(solution("3people unFollowed me"));
+		System.out.println(solution("3people unFollowed  me "));
 		System.out.println(solution("for the last week"));
 	}
 
