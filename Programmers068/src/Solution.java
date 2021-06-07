02b0927b4972e6c9606cd849e97d00ba61258624
yerim <pppp5097@naver.com>
2021-06-07 21:00:17 +0900

    Java2-21
    영어 끝말잇기

--- Programmers068/src/Solution.java
+++ Programmers068/src/Solution.java
@@ -1,17 +1,17 @@
-
+import java.util.ArrayList;
 public class Solution {
     public static int[] solution(int n, String[] words) {
         int[] answer = new int[2];
-        
+        ArrayList<String> arr = new ArrayList<>();
+        arr.add(words[0]);
         for(int i=1; i<words.length; i++) {
-        	if(words[i].charAt(0)!=words[i-1].charAt(words[i-1].length()-1)) {
-        		answer[0] = (i+1)/n + 1;
-        		answer[1] = (i+1)%n + 1;
+        	if(words[i].charAt(0)!=words[i-1].charAt(words[i-1].length()-1)||arr.contains(words[i])) {
+        		answer[1] = i/n + 1;
+        		answer[0] = i%n + 1;
         		break;
         	}
+        	arr.add(words[i]);
         }
-
-
         return answer;
     }
 	public static void main(String[] args) {
