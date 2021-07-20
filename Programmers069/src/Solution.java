c6443bea8820dead50066aa88a083c12e49c5623
yerim <pppp5097@naver.com>
2021-07-21 00:05:26 +0900

    0 제거하기

--- Programmers069/src/Solution.java
+++ Programmers069/src/Solution.java
@@ -13,23 +13,15 @@ public class Solution {
         
       
 //        1. 0 제거 + 갯수 COUNT
-//        for(int i=0; i<arr.size(); i++) {
-//        	if(arr.get(i).equals("0")) {
-//        		arr.remove(i--);
-//        		zero_cnt++;
-//        	}
-//        }
-        for(int i=0; i<s.length(); i++) {
+        for(int i=0; i<s.length(); ++i) {
         	if(s.substring(i, i+1).equals("0")) {
-        		System.out.println(i);
         		if(i==0) s = s.substring(1, s.length()-1);
-        		else s = s.substring(0, i-1) + s.substring(i+1, s.length()-1);
-        		zero_cnt++; i--;
+        		else s = s.substring(0, i) + s.substring(i+1, s.length());
+        		zero_cnt++; --i;
         	}
         }
         
 //        2. 2진수로 변환
-        
         
         answer[1] = zero_cnt;
         
