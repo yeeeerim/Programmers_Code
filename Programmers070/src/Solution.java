4d72b567d2b4bd5c715e94c173fe4ce5f033caac
yerim <pppp5097@naver.com>
2021-08-12 23:51:05 +0900

    java1-48
    ing

--- Programmers070/src/Solution.java
+++ Programmers070/src/Solution.java
@@ -1,15 +1,19 @@
-import java.util.ArrayList;
 
-// 방문길이
+// 1단계 (숫자 문자열과 영단어)
 public class Solution {
-    public static int solution(String dirs) {
+    public static int solution(String s) {
         int answer = 0;
-        ArrayList<Integer> arr = new ArrayList<>();
+        
+        for(int i=0; i<s.length(); i++) {
+        	if(!Character.isDigit(s.charAt(i))){
+        		
+        	}
+        }
         
         return answer;
     }
 	public static void main(String[] args) {
-		System.out.println(solution("ULURRDLLU"));
+		System.out.println(solution("one4seveneight"));
 	}
 
 }
