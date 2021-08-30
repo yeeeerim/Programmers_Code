bde86bff9dae3af4fefa76903f4ee070c1080ff2
yerim <pppp5097@naver.com>
2021-08-30 14:07:07 +0900

    Java1-52
    약수의 개수와 덧셈

---
+++ Programmers074/src/Solution.java
@@ -0,0 +1,23 @@
+// 약수의 개수와 덧셈 
+
+public class Solution {
+    public static int solution(int left, int right) {
+        int answer = 0;
+        
+        for(int i=left; i<=right; i++) {
+        	int cnt = 0;
+        	for(int j=1; j<=i; j++) {
+        		if(i%j==0) cnt++;
+        	}
+        	if(cnt%2==1) answer-=i;
+        	else answer+=i;
+        }
+        
+        return answer;
+    }
+
+	public static void main(String[] args) {
+		System.out.println(solution(13,17));
+	}
+
+}
