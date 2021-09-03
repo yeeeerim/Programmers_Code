395ec43d5ffb0cdc6b3d3a87c060c0a76fbd24f5
yerim <pppp5097@ynu.ac.kr>
2021-09-03 20:23:57 +0900

    Java2-23
    괄호 변환

--- Programmers076/src/Solution.java
+++ Programmers076/src/Solution.java
@@ -3,33 +3,56 @@
 public class Solution {
     public static String solution(String p) {
         String answer = "";
-
+        String[] pair = new String[2];
         
-        run(p);
+        pair[0] = ""; pair[1] = "";
         
-       
-        if(!isBalance(u)) {
-        	String s = "(";
-        	//재귀
-        	s+=")";
-        	u = u.substring(1, u.length());
-        	for(int i=0; i<u.length(); i++) {
-        		if(u.substring(i, i+1).equals("(")) s+=")";
-        		else s+="(";
-        	}
+        while(true) {
+        	
+            pair = run(p); // 1,2번
+            
+            if(pair[1].length()==0&&pair[0].length()==0) break;
+            
+            if(isRight(pair[0])) {
+            	answer+=pair[0];
+            	p = pair[1];
+            	
+            	continue;
+            } else {
+            	String s = "(";
+            	
+            	//재귀
+            	s += solution(pair[1]);
+            	
+            	s+=")";
+            	
+            	pair[0] = pair[0].substring(1, pair[0].length()-1);
+            	for(int i=0; i<pair[0].length(); i++) {
+            		if(pair[0].substring(i, i+1).equals("(")) s+=")";
+            		else s+="(";
+            	}
+            	
+            	answer+=s;
+            	
+            	break;
+            }
         }
-        
-        System.out.println(u+","+v);
 
         
         return answer;
     }
 
-	private static String run(String p) {
+	private static String[] run(String p) {
 		int left_cnt=0, right_cnt=0;
 		String u="", v="";
-		if(p.length()==0) return "";
+		String answer[] = new String[2];
 		
+		answer[0]=""; answer[1]="";
+		
+		// 1. 입력이 빈 문자열인 경우, 빈 문자열 반환
+		if(p.length()==0) return answer;
+		
+		// 2. 문자열 p를 u, v로 분리
 		for(int i=0; i<p.length(); i++) {
 			if(p.substring(i, i+1).equals("(")) left_cnt++;
 			else right_cnt++;
@@ -39,9 +62,14 @@ public class Solution {
 				break;
 			}
 		}
+		
+		answer[0] = u;
+		answer[1] = v;
+		
+		return answer;
 	}
 
-	private static boolean isBalance(String u) {
+	private static boolean isRight(String u) {
 		int stack = 0;
 		
 		for(int i=0; i<u.length(); i++) {
@@ -56,6 +84,8 @@ public class Solution {
 
 	public static void main(String[] args) {
 		System.out.println(solution("(()())()"));
+		System.out.println(solution(")("));
+		System.out.println(solution("()))((()"));
 	}
 
 }
