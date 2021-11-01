5021eb97e7a68be9b19d0bf8992db7381b0e3712
yerim <pppp5097@naver.com>
2021-11-01 13:49:28 +0900

    Java2-9
    멀쩡한 사각형

--- Programmers056/src/Solution.java
+++ Programmers056/src/Solution.java
@@ -1,12 +1,12 @@
 
 public class Solution {
-    public static long solution(int w, int h) {
-        long answer = (long)w*h;
-        if(w>h) {int temp = w; w=h; h=temp;}
-        double slope = (double)h/w;
+    public static long solution(long w, long h) {
+        long answer = w*h;
+        long cut = 0;
+        if(w>h) {long temp = w; w=h; h=temp;}
         for(int i=1; i<=w; i++)
-        	answer-=Math.ceil(slope*i) - Math.floor(slope*(i-1));
-        return answer;
+        	cut+=Math.ceil((double)h*i/w) - Math.floor((double)h*(i-1)/w);
+        return answer-cut;
     }
 	public static void main(String[] args) {
 		System.out.println(solution(11,8));
