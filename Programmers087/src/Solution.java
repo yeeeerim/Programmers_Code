30f815216928469c63ce0c02d0c49c7f44c9797b
yerim <pppp5097@ynu.ac.kr>
2021-11-05 16:45:38 +0900

    Java2-30
    소수 찾기
    (ing)

---
+++ Programmers087/src/Solution.java
@@ -0,0 +1,14 @@
+
+public class Solution {
+    public static int solution(String numbers) {
+        int answer = 0;
+        
+        
+        
+        return answer;
+    }
+	public static void main(String[] args) {
+		System.out.println(solution("17"));
+	}
+
+}
