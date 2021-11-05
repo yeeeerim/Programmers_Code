e0f29a5e3e69bd56284af27d0f6d7aef157a85f9
yerim <pppp5097@ynu.ac.kr>
2021-11-05 16:04:34 +0900

    Java2-11
    가장 큰 수

--- Programmers058/src/Solution.java
+++ Programmers058/src/Solution.java
@@ -1,44 +1,40 @@
+import java.util.*;
 
-public class Solution {
+public class Solution implements Comparator<String> {
+
+	@Override
+	public int compare(String s1, String s2) {
+		String temp1 = s1+s2, temp2 = s2+s1;
+		
+		return temp2.compareTo(temp1);
+	}
+	
     public static String solution(int[] numbers) {
         String answer = "";
         int len = numbers.length;
         String[] nums = new String[len];
+        int sum = 0;
         
-        for(int i=0; i<len; i++)
+        for(int i=0; i<len; i++) {
         	nums[i]=numbers[i]+"";
-        
-        for(int i=0; i<len-1; i++) {
-        	for(int j=i+1; j<len; j++) {
-        		if(comp(nums[i],nums[j])<1) {
-        			String temp = nums[i];
-        			nums[i] = nums[j];
-        			nums[j] = temp;
-        		}
-        	}
+        	sum+=numbers[i];
         }
+        
+        Arrays.sort(nums, new Solution());
+        
+        if(sum==0) return "0";
+        
         for(int i=0; i<len; i++) {
         	answer+=nums[i];
         }
         return answer;
     }
     
-    private static int comp(String s1, String s2) {
-    	if(s1.length()==s2.length()) {
-			return s1.compareTo(s2);//길이 같으면 사전순으로
-    	}
-		if(s1.length()>s2.length()) {// 길이 비교
-			return 1;
-		} else {
-			return -1;
-		}
-	}
-    
 	public static void main(String[] args) {
-		int[] n = {6,10,2};
+		int[] n = {3,30,34,5,9};
 		System.out.println(solution(n));
-		System.out.println("321".compareTo("3"));
 	}
+
 
 
 }
