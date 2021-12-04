90b8248e1fec92e24f8f4f8e6508e0107e4253f3
yerim <pppp5097@ynu.ac.kr>
2021-12-04 16:22:09 +0900

    Java2-39
    삼각 달팽이

--- Programmers096/src/Solution.java
+++ Programmers096/src/Solution.java
@@ -5,29 +5,29 @@ public class Solution {
     	int max = (n*n+n)/2;
         int[] answer = new int[max];
         int[][] arr = new int[n][n];
-        int cnt = 1;
-        int start = 0, end = n;
+        int start_x = 0, start_y = 0;
+        int end_x = n-1, end_y = n-1;
+        int[][] move = {{0,1},{1,0},{-1,-1}};
+        int x = 0, y = 0;
         
-        while(true) {
-            for(int i=start; i<end; i++) { // 아래로
-            	arr[i][start] = cnt++; System.out.println("↓"+(cnt-1));if(cnt>max) break;
-            } if(cnt>max) break;
-            for(int i=start+1; i<end; i++) { // 오른쪽으로
-            	arr[end-1][i] = cnt++; System.out.println("→"+(cnt-1));if(cnt>max) break;
-            } if(cnt>max) break;
-            for(int i=end-2; i>start; i--) { // 대각선으로
-            	arr[i][i] = cnt++; System.out.println("↖"+(cnt-1));if(cnt>max) break;
-            } if(cnt>max) break;
-            start++; end--;
-        }
-
-        for(int i=0; i<n; i++) {
-        	for(int j=0; j<n; j++) {
-        		System.out.print(arr[i][j]+"\t");
+        int move_x = move[0][0], move_y = move[0][1];
+        
+        for(int i=1; i<=max; i++) {
+        	arr[y][x] = i;
+        	
+        	if(x==start_x&&y==end_y) {
+        		move_x = move[1][0]; move_y = move[1][1];
+        	} else if(x==end_x&&y==end_y) {
+        		move_x = move[2][0]; move_y = move[2][1];
+        	} else if(x==start_x+1&&y==start_y+1) {
+        		move_x = move[0][0]; move_y = move[0][1];
+        		start_x++; start_y+=2;
+        		end_x-=2; end_y--;
         	}
-        	System.out.println();
+        	
+        	x += move_x;
+        	y += move_y;
         }
-        
         
         int index = 0;
         for(int i=0; i<n; i++) {
@@ -40,7 +40,7 @@ public class Solution {
     }
     
 	public static void main(String[] args) {
-		int[] answer = solution(4);
+		int[] answer = solution(6);
 		for(int i=0; i<answer.length; i++) {
 			System.out.print(answer[i]+" ");
 		}
