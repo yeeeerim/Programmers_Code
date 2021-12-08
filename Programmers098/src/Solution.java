411cb1d2837a6fada7de9f988e5477b9f8fddf67
yerim <pppp5097@ynu.ac.kr>
2021-12-08 16:34:02 +0900

    Java2-41
    n^2 배열 자르기
    (정확성 55)

--- Programmers098/src/Solution.java
+++ Programmers098/src/Solution.java
@@ -1,25 +1,24 @@
+import java.util.Arrays;
+
 // n^2 배열 자르기
 
 public class Solution {
     public static int[] solution(int n, long left, long right) {
-    	int len = (int)right - (int)left + 1;
-        int[] answer = new int[len];
-        
         int k = 0;
+        int left_row = (int)left/n, left_column = (int)left%n;
+        int right_row = (int)right/n, right_column = (int)right%n;
+        int[] answer = new int[(right_row-left_row+1)*n];
         
-        
-        for(int i=(int)left/n; i<(int)right/n; i++) {
-        	for(int j=(int)left%n; j<(int)right%n; j++) {
+        for(int i=left_row; i<=right_row; i++) {
+        	for(int j=0; j<n; j++) {
     			answer[k++] = (i<j)?j+1:i+1;
-    			System.out.println(i+","+j);
         	}
         }
         
-        
-        return answer;
+        return Arrays.copyOfRange(answer, left_column, (int)right - n*left_row + 1);
     }
 	public static void main(String[] args) {
-		int[] answer = solution(4,7,14);
+		int[] answer = solution(2,0,3);
 		
 		for(int i=0; i<answer.length; i++) {
 			System.out.print(answer[i]+" ");
