89683a87416ed8bd9b22fdd6756704866a1f3d86
yerim <pppp5097@naver.com>
2021-12-09 23:57:07 +0900

    Java2-43
    교점에 별 만들기
    (ing)

---
+++ Programmers100/src/Solution.java
@@ -0,0 +1,22 @@
+
+public class Solution {
+    public static String[] solution(int[][] line) {
+        String[] answer = {};
+        
+        for(int i=0; i<line.length; i++) {
+        	for(int )
+        }
+        getIntersection();
+        
+        return answer;
+    }
+	public static void main(String[] args) {
+		int[][] line = {{2,-1,4},{-2,-1,4},{0,-1,1},{5,-8,-12},{5,8,12}};
+		String[] s = solution(line);
+		
+		for(int i=0; i<s.length; i++)
+			System.out.println(s[i]);
+		
+	}
+
+}
